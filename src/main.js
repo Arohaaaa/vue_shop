982fe4adeaf719052ac14a66f1f7460f484f47fb
Arohaaaa <632681127@qq.com>
2020-07-02 16:06:30 +0800

    Enter

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ import 'quill/dist/quill.bubble.css'
 
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
   return config
